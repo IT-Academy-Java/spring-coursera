c5c3b75cc58b9db886625861e492f72413ead968
Elena Piaggio <elenapiaggio@gmail.com>
2021-09-27 18:00:21 +0200

    ADD StudentDao

--- src/main/java/com/example/coursera/dao/StudentDao.java
+++ src/main/java/com/example/coursera/dao/StudentDao.java
@@ -1,2 +1,12 @@
-package com.example.coursera.dao;public class StudentDao {
+package com.example.coursera.dao;
+
+import com.example.coursera.model.Student;
+
+import java.util.Collection;
+
+public interface StudentDao {
+
+  Student getOne(String id);
+  Collection<Student> getAll();
 }
+
