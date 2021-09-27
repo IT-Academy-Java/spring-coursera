9851917592b89177702893184d05c4c9fdc599a6
Elena Piaggio <elenapiaggio@gmail.com>
2021-09-27 18:00:00 +0200

    ADD service implement

--- src/main/java/com/example/coursera/service/StudentService.java
+++ src/main/java/com/example/coursera/service/StudentService.java
@@ -1,2 +1,12 @@
-package com.example.coursera.service;public class StudentService {
+package com.example.coursera.service;
+
+import com.example.coursera.model.Student;
+
+import java.util.Collection;
+
+public interface StudentService {
+
+  Student getStudent(String id);
+  Collection<Student> getAllStudents();
+
 }
