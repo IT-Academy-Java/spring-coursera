9629e2e3607c069aaa2c1a216d4dd4762c77116d
Elena Piaggio <elenapiaggio@gmail.com>
2021-09-27 18:01:12 +0200

    ADD Student Controller

--- src/main/java/com/example/coursera/controller/StudentController.java
+++ src/main/java/com/example/coursera/controller/StudentController.java
@@ -1,9 +1,14 @@
 package com.example.coursera.controller;
 
+import com.example.coursera.model.Student;
+import com.example.coursera.service.StudentService;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.inject.Inject;
+import java.util.Collection;
 
 @RequestMapping("/student")
 @RestController
@@ -12,8 +17,16 @@ public class StudentController {
   @Value("${message}")
   private String strController;
 
+  @Inject
+  private StudentService studentService;
+
   @GetMapping("/msg")
   public String getStrController(){
     return strController;
+  }
+
+  @GetMapping("/all")
+  public Collection<Student> getAll(){
+    return studentService.getAllStudents();
   }
 }
